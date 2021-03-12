a84072d071b5f71d287fdccbe681223b5a23c064
aryotech <aryofreeyoung@gmail.com>
2021-03-12 18:03:57 +0700

    property class atau variable bisa ditulis pada parameter sekaligus menjadi Constructor

--- src/com/kelas_coding/User.kt
+++ src/com/kelas_coding/User.kt
@@ -1,6 +1,6 @@
 package com.kelas_coding
 
-class User (var userName : String, var passWord : String) { //property class atau variable bisa ditulis pada parameter
+class User (var userName : String = "Ali", var passWord : String = "1111") { //property class atau variable bisa ditulis pada parameter
 
    // var username : String = userName
     //var password: String = passWord
@@ -9,8 +9,16 @@ class User (var userName : String, var passWord : String) { //property class ata
 
 fun main() {
 
-    val obj1 = User(userName = "Ali", passWord = "1111")
+   val obj1 = User(userName="ali", passWord = "1111")
+    val obj2 = User(userName = "Andi")
+    val obj3 = User()
 
     println("username : ${obj1.userName}")
     println("password : ${obj1.passWord}")
+
+    println("username : ${obj2.userName}")
+    println("password : ${obj2.passWord}")
+
+    println("username : ${obj3.userName}")
+    println("password : ${obj3.passWord}")
 }
